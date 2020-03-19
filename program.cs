aa3dea0ac99bc0259f9e84d86cc563686acfa1d1
mfierd <muhammad.2789@students.amikom.ac.id>
2020-03-19 22:36:13 +0700

    program-taxi

---
+++ program.cs
@@ -0,0 +1,27 @@
+//Program.cs
+using System;
+
+
+namespace Taxi {
+    class Program {
+
+        static void Main(string[] args) {
+
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+          
+        }
+
+    }
+
+}
